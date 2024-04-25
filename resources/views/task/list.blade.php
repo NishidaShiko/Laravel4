ddbfc402b3d4140cd6b323f027e8e36414c916d3
NishidaShiko <ec2-user@ip-172-31-42-81.ap-northeast-1.compute.internal>
2024-04-25 06:05:06 +0000

    404修正後再度404

--- resources/views/task/list.blade.php
+++ resources/views/task/list.blade.php
@@ -21,8 +21,8 @@
                 タスク名:<input name="name" value="{{ old('name') }}"><br>
                 期限:<input name="period" type="date" value="{{ old('period') }}"><br>
                 タスク詳細:<textarea name="detail">{{ old('detail') }}</textarea><br>
-                重要度:<label><input type="radio" name="priority" value="1" @if (old('priority') == 1) checked @endif>低い</label> / 
-                    <label><input type="radio" name="priority" value="2" @if (old('priority', 2) == 2) checked @endif>普通</label> / 
+                重要度:<label><input type="radio" name="priority" value="1" @if (old('priority') == 1) checked @endif>低い</label> /
+                    <label><input type="radio" name="priority" value="2" @if (old('priority', 2) == 2) checked @endif>普通</label> /
                     <label><input type="radio" name="priority" value="3" @if (old('priority') == 3) checked @endif>高い</label><br>
                 <button>タスクを登録する</button>
             </form>
@@ -39,7 +39,7 @@
             <td>{{ $task->name }}
             <td>{{ $task->period }}
             <td>{{ $task->getPriorityString() }}
-            <td><a href="./detail.html">詳細閲覧</a>
+<td><a href="{{ route('detail', ['task_id' => $task->id]) }}">詳細閲覧</a>
             <td><a href="./edit.html">編集</a>
             <td><form action="./top.html"><button>完了</button></form></a>
 @endforeach
